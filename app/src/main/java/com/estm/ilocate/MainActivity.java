2ba8fcee4f611fd5996780078da89660c12b4366
ammar <ammar.sultan@edu.umi.ac.ma>
2021-11-09 23:30:32 +0300

    add mapview to ilocate and ifind activities.

--- app/src/main/java/com/estm/ilocate/MainActivity.java
+++ app/src/main/java/com/estm/ilocate/MainActivity.java
@@ -2,9 +2,15 @@ package com.estm.ilocate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonilocate;
+    private Button buttonifind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,30 @@ public class MainActivity extends AppCompatActivity {
         // Salam this is the first comment.
         // In this project Inchalah we will be working on android
         // Studio as an IDE for creating our iLocate application
-        // ENJOY.
+
+        //setting buttons
+        buttonilocate = (Button) findViewById(R.id.buttonilocate);
+        buttonifind = (Button) findViewById(R.id.buttonifind);
+
+        //setting onclicklistener for ilocate button
+        buttonilocate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this.getBaseContext(), iLocateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //setting onclicklistener for ifind button
+        buttonifind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this.getBaseContext(), iFindActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
