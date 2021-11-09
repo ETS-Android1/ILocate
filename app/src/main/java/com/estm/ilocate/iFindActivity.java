e002b627c009bf5cf81f9d27b48738d7ecf37b40
ammar <ammar.sultan@edu.umi.ac.ma>
2021-11-09 14:22:13 +0300

    add necessary buttons.

--- app/src/main/java/com/estm/ilocate/iFindActivity.java
+++ app/src/main/java/com/estm/ilocate/iFindActivity.java
@@ -1,4 +1,26 @@
 package com.estm.ilocate;
 
-public class iFindActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class iFindActivity extends Activity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_ifind);
+
+        //setting buttons
+        button1 = (Button) findViewById(R.id.button4);
+        button2 = (Button) findViewById(R.id.button5);
+        button3 = (Button) findViewById(R.id.button6);
+    }
 }
--- app/src/main/java/com/estm/ilocate/iLocateActivity.java
+++ app/src/main/java/com/estm/ilocate/iLocateActivity.java
@@ -1,4 +1,25 @@
 package com.estm.ilocate;
 
-public class iLocateActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class iLocateActivity extends Activity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ilocate);
+
+        //setting buttons for iLocate activity
+        button1 = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+    }
 }
