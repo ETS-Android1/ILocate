b7d9b852c5e7452eede737d4e4b9031287a383dc
ammar <ammar.sultan@edu.umi.ac.ma>
2022-04-27 23:58:00 +0000

    add splash screen

---
+++ app/src/main/java/com/estm/ilocate/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.estm.ilocate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(SplashActivity.this.getBaseContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}
