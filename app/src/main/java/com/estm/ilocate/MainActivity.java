deeeb41b1d7b81e53f05825a280ae9a7d5d6f09e
ammar <ammar.sultan@edu.umi.ac.ma>
2022-05-09 12:47:10 +0100

    map

--- app/src/main/java/com/estm/ilocate/MainActivity.java
+++ app/src/main/java/com/estm/ilocate/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         buttonMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this.getBaseContext(), MapsActivity.class);
+                Intent intent = new Intent(MainActivity.this.getBaseContext(),  MapsActivity.class);
                 startActivity(intent);
             }
         });
