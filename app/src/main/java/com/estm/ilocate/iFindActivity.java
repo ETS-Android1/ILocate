a54ccb342f33653179106a6cf87464863cc2e182
ammar <ammar.sultan@edu.umi.ac.ma>
2022-05-15 15:26:32 +0100

    add map to ilocate and ifind activities

--- app/src/main/java/com/estm/ilocate/iFindActivity.java
+++ app/src/main/java/com/estm/ilocate/iFindActivity.java
@@ -3,17 +3,26 @@ package com.estm.ilocate;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class iFindActivity extends Activity {
+public class iFindActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private MapView mapView;
+    private ImageButton button4;
+    private ImageButton button5;
+    private ImageButton button6;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,10 +30,34 @@ public class iFindActivity extends Activity {
 
         setContentView(R.layout.activity_ifind);
 
-        mapView = (MapView) findViewById(R.id.mapView2);
         //setting buttons
-        button1 = (Button) findViewById(R.id.button4);
-        button2 = (Button) findViewById(R.id.button5);
-        button3 = (Button) findViewById(R.id.button6);
+        button4 = (ImageButton) findViewById(R.id.imageButton4);
+        button5 = (ImageButton) findViewById(R.id.imageButton5);
+        button6 = (ImageButton) findViewById(R.id.imageButton6);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map3);
+        mapFragment.getMapAsync(this);
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     *
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- app/src/main/java/com/estm/ilocate/iLocateActivity.java
+++ app/src/main/java/com/estm/ilocate/iLocateActivity.java
@@ -3,28 +3,59 @@ package com.estm.ilocate;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class iLocateActivity extends Activity {
+public class iLocateActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private MapView mapView;
+    private ImageButton button;
+    private ImageButton button2;
+    private ImageButton button3;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ilocate);
-
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.setActivated(true);
         //setting buttons for iLocate activity
-        button1 = (Button) findViewById(R.id.button);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
+        button = (ImageButton) findViewById(R.id.imageButton);
+        button2 = (ImageButton) findViewById(R.id.imageButton2);
+        button3 = (ImageButton) findViewById(R.id.imageButton3);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map2);
+        mapFragment.getMapAsync(this);
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     *
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
